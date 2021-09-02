103873f5da797138644e03cdb9af150fc6522c88
sandman <bmsandoval@gmail.com>
2021-09-01 18:50:00 -0600

    stubbing user groups capabilities

--- db/migrations/20210826061900_create_users_table.sql
+++ db/migrations/20210826061900_create_users_table.sql
@@ -1,21 +1,21 @@
 -- +goose Up
 CREATE TABLE IF NOT EXISTS `users` (
-   id binary(16) PRIMARY KEY,
-   sub varchar(36) generated always as
-      (insert(
-          insert(
-              insert(
-                  insert(hex(id),9,0,'-'),
-                  14,0,'-'),
-              19,0,'-'),
-          24,0,'-')
-       ) virtual,
-   username VARCHAR(75),
-   UNIQUE KEY unique_username (username),
-   password VARCHAR(75),
-   created_at  DATETIME DEFAULT NOW(),
-   updated_at  DATETIME DEFAULT NOW(),
-   deleted_at  DATETIME
+    id binary(16) PRIMARY KEY,
+    sub varchar(36) generated always as
+    (insert(
+        insert(
+            insert(
+                insert(hex(id),9,0,'-'),
+                14,0,'-'),
+            19,0,'-'),
+        24,0,'-')
+    ) virtual,
+    username VARCHAR(75),
+    UNIQUE KEY unique_username (username),
+    password VARCHAR(75),
+    created_at  DATETIME DEFAULT NOW(),
+    updated_at  DATETIME DEFAULT NOW(),
+    deleted_at  DATETIME
 );
 -- +goose Down
 DROP TABLE IF EXISTS users
